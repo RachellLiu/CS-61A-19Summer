16bf84040aee4d0cbbc8bf3b725081618ed1a558
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2021-10-11 18:08:58 +0100

    Add files via upload

---
+++ lab06/lab06.py
@@ -0,0 +1,34 @@
+
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    counter = -1
+    def adder(x):
+        nonlocal counter
+        counter += 1
+        return n + x + counter
+    return adder
+
+def map(fn, lst):
+    """Maps fn onto lst using mutation.
+    >>> original_list = [5, -1, 2, 0]
+    >>> map(lambda x: x * x, original_list)
+    >>> original_list
+    [25, 1, 4, 0]
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(len(lst)):
+        lst[i] = fn(lst[i])
